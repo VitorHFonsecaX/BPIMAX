49a456d2bbf89aba898966dde5ef432978fe3009
VitorHFonsecaX <vitorhfonseca01@hotmail.com>
2025-06-12 14:38:06 -0300

    Atualizar

--- Inicializador.py
+++ Inicializador.py
@@ -1,6 +1,8 @@
-from PySide6.QtWidgets import QApplication
 import sys
-from App.Janela_principal import JanelaPrincipal
+# import os # Não é necessário para este arquivo
+
+from PySide6.QtWidgets import QApplication
+from App.Janela_principal import JanelaPrincipal # Assumindo que Janela_principal.py está dentro de App/
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
