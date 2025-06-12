def54fe5693ef2fb66969a229c0631ebd58f3259
VitorHFonsecaX <vitorhfonseca01@hotmail.com>
2025-06-12 14:22:12 -0300

    Atualizar

--- Inicializador.py
+++ Inicializador.py
@@ -1,6 +1,6 @@
 import sys
 from PySide6.QtWidgets import QApplication
-from app.autenticacao.ui_login import TelaLogin  # você vai criar isso já
+from App.Autentificacao.Ui_login import TelaLogin  # você vai criar isso já
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
