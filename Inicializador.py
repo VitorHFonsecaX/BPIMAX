949f08a806c7bc27e44ee70a76e3bc0707344328
VitorHFonsecaX <vitorhfonseca01@hotmail.com>
2025-06-13 09:19:02 -0300

    Atualização

--- Inicializador.py
+++ Inicializador.py
@@ -2,7 +2,7 @@ import sys
 # import os # Não é necessário para este arquivo
 
 from PySide6.QtWidgets import QApplication
-from App.Janela_principal import JanelaPrincipal # Assumindo que Janela_principal.py está dentro de App/
+from BPIMAX.UI.Janela_principal import JanelaPrincipal # Assumindo que Janela_principal.py está dentro de App/
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
